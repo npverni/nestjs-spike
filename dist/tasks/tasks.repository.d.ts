830119ff2a147c0d2b8b81ef27bc567cf5719356
Nathan Verni <npverni@gmail.com>
2021-09-06 12:24:01 -0400

    wip

--- dist/tasks/tasks.repository.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTaskFilterDto } from './dto/get-tasks-filter';
-import { Task } from './task.entity';
-export declare class TasksRepository extends Repository<Task> {
-    getTasks(filterDto: GetTaskFilterDto): Promise<Task[]>;
-    createTask(createTaskDto: CreateTaskDto): Promise<Task>;
-}
